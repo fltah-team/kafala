5afe57bc46587c41befe8a60a79c9bc2d8043633
ameenah <ameenah.k.a.m@gmail.com>
2016-04-13 19:07:46 +0300

    update studentConnection(remove sibiling field) + update on DB

--- employee/orphan/saveSibiling.php
+++ employee/orphan/saveSibiling.php
@@ -4,14 +4,14 @@
 	include('../../utils/db.php');
 	include('../../utils/orphanAPI.php');
 	
-	
+	$orphan_id = 2 ;
 	$name  = "ss" ;
 	$sex = "male";
 	$birth_date  =  "2000-12-10";
 	$state = "";
 	
 	
-	$result = fp_sibiling_add($name , $sex , $birth_date , $state) ;
+	$result = fp_sibiling_add( $orphan_id ,$name , $sex , $birth_date , $state) ;
 	
 
 	fp_db_close();
